ae8378e6dd7e8b8773c719d8f2df1744c24facd0
John Fáwọlé <johnfawole18@gmail.com>
2023-04-10 04:18:52 +0100

    Create How to Build and Deploy Flashloan Contracts on Celo with Aave.sol

---
+++ How to Build and Deploy Flashloan Contracts on Celo with Aave.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { FlashLoanReceiverBase } from "./FlashLoanReceiverBase.sol";
+import { ILendingPool, ILendingPoolAddressesProvider, IERC20 } from "./Interfaces.sol";
+import { SafeMath } from "./Libraries.sol";
+import "./Ownable.sol";
+/** 
+    !!!
+    Never keep funds permanently on your FlashLoanReceiverBase contract as they could be 
+    exposed to a 'griefing' attack, where an attacker uses the stored funds.
+    !!!
+ */
+contract MyV2FlashLoan is FlashLoanReceiverBase, Ownable {
+    using SafeMath for uint256;
+
+    constructor(ILendingPoolAddressesProvider _addressProvider) FlashLoanReceiverBase(_addressProvider) {
+        function myFlashLoanCall(address _tokenAddress) public onlyOwner {
+        address receiverAddress = address(this);
+
+        address[] memory assets = new address[](1);
+        assets[0] = address(_tokenAddress); //goerli USDC 0x9FD21bE27A2B059a288229361E2fA632D8D2d074
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1*10**6;
+
+        // 0 = no debt, 1 = stable, 2 = variable
+        uint256[] memory modes = new uint256[](1);
+        modes[0] = 0;
+
+        address onBehalfOf = address(this);
+        bytes memory params = "";
+        uint16 referralCode = 0;
+
+        LENDING_POOL.flashLoan(
+            receiverAddress,
+            assets,
+            amounts,
+            modes,
+            onBehalfOf,
+            params,
+            referralCode
+        );
+    }
+}
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    )
+        external
+        override
+        returns (bool)
+    {
+
+        //
+        // This contract now has the funds requested.
+        // Your logic goes here.
+        //
+
+        // At the end of your logic above, this contract owes
+        // the flash loaned amounts + premiums.
+        // Therefore ensure your contract has enough to repay
+        // these amounts.
+
+        // Approve the LendingPool contract allowance to *pull* the owed amount
+        for (uint i = 0; i < assets.length; i++) {
+            uint amountOwing = amounts[i].add(premiums[i]);
+            IERC20(assets[i]).approve(address(LENDING_POOL), amountOwing);
+        }
+
+        return true;
+    }
+
+    
+    
+    }
